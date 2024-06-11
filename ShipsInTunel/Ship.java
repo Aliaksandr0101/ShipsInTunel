4ceca54ba9628b0521c76b18f1894224463a8b33
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-06-11 23:08:32 +0300

    TaskWithShips

---
+++ ShipsInTunel/Ship.java
@@ -0,0 +1,35 @@
+package ShipsInTunel;
+
+public class Ship {
+    private int count;
+    private int size;
+    private int type;
+
+    public Ship(Size size, Type type) {
+        this.size = size;
+        this.type = type;
+    }
+
+    public void add(int count) {
+        this.count += count;
+    }
+
+    public boolean countCheck() {
+        if (count >= size.getValue()) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+}
