5573e1a768765166402f7bb88e89c019a28c2b4e
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-06-20 17:07:44 +0300

    TaskWithShips

--- ShipsInTunnelNew/Tongue.java
+++ ShipsInTunnelNew/Tongue.java
@@ -4,5 +4,6 @@ public class Tongue {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("hello");
+        System.out.println("my friend");
     }
 }
