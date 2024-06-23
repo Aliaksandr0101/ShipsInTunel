aab539c214bf5cb4aa2e1d379da8a3266b144924
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-06-23 09:50:28 +0300

    TaskWithShips

---
+++ ShipsInTunnelNew/AppTest.java
@@ -0,0 +1,9 @@
+package ShipsInTunnelNew;
+
+public class AppTest {
+    
+    static void setup(){
+        System.out.println("@BeforeAll executed");
+    }
+
+}
--- ShipsInTunnelNew/Tongue.java
+++ ShipsInTunnelNew/Tongue.java
@@ -2,9 +2,7 @@ package ShipsInTunnelNew;
 
 public class Tongue {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("hello");
-        System.out.println("my friend");
-        System.out.println("my friend");
+
     }
+
 }
